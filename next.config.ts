0bec92e135265ab3db6132501823185f63405f7e
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2025-11-25 23:31:43 -0500

    test deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/chatherine",
+  assetPrefix: "/chatherine/",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
